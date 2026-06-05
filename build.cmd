c1895ca01c5d1cd9cb44efccac3c71cc3ba48d06
Sassy Lily <212339561+sassy-lily@users.noreply.github.com>
2026-06-06 00:30:06 +0200

    remove previous implementation

--- build.cmd
+++
@@ -1,4 +0,0 @@
-CALL ".venv\Scripts\activate.bat"
-pyinstaller -F src\mangadex_follows_exporter.py
-COPY configuration.ini dist
-COPY mangaupdates.json dist
--- prepare.cmd
+++
@@ -1,5 +0,0 @@
-git reset --hard
-git clean -d -f -x
-python -m venv .venv
-CALL ".venv/Scripts/activate.bat"
-python -m pip install -r requirements.txt
